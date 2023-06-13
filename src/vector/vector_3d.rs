11d6e4255dbf54e3cfd3c2c37b41e6f9b156739d
Akamitori <akamitori@tutanota.com>
2023-06-13 18:12:31 +0300

    Implemented Projection and Rejection for matrix 3D

--- src/vector/vector_3d.rs
+++ src/vector/vector_3d.rs
@@ -1,9 +1,13 @@
-﻿#[derive(Default, Debug, Clone, Copy)]
+﻿use crate::vector::vector_3d_operations::Dot;
+
+#[derive(Default, Debug, Clone, Copy)]
 pub struct Vector3d {
     pub x: f64,
     pub y: f64,
     pub z: f64,
 }
+
+
 
 impl Vector3d {
     pub fn new(x: f64, y: f64, z: f64) -> Self {
@@ -14,13 +18,24 @@ impl Vector3d {
         }
     }
 
+    #[inline(always)]
     pub fn magnitude(&self) -> f64 {
-        (self.x * self.x + self.y * self.y + self.z * self.z).sqrt()
+    (self.x * self.x + self.y * self.y + self.z * self.z).sqrt()
     }
-    
+
+    #[inline(always)]
     pub fn normalize(&self) -> Vector3d {
-        return  self.clone()/self.magnitude()
+        return self.clone() / self.magnitude();
     }
-    
+
+    #[inline(always)]
+    pub fn Project(&self, to: &Vector3d) -> Vector3d {
+        return *to * Dot(self, to) / Dot(to, to);
+    }
+
+    #[inline(always)]
+    pub fn Reject(&self, from: &Vector3d) -> Vector3d {
+        return *self - *from * Dot(self, from) / Dot(from, from);
+    }
 }
 
